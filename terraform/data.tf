28aaae70c9ddfd19c07026aa62cbfbd279708a1d
Jon Brouse <jbrouse19@gmail.com>
2017-02-06 23:30:57 +0000

    udpated bucket name

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "account" {
   backend = "s3"
 
   config {
-    bucket = "${var.aws_account}"
+    bucket = "singlecomm-terraform-state-${var.aws_account}"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
@@ -12,7 +12,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config {
-    bucket = "${var.aws_account}"
+    bucket = "singlecomm-terraform-state-${var.aws_account"
     key    = "${var.aws_region}/${var.vpc_name}/terraform.tfstate"
     region = "us-east-1"
   }
