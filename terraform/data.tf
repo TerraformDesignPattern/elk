7bde4eef2d9bc78e4b10dec3907db1f42b5f9f74
Jon Brouse <jbrouse19@gmail.com>
2017-05-09 21:33:24 -0400

    Added profile to data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,9 +2,10 @@ data "terraform_remote_state" "account" {
   backend = "s3"
 
   config {
-    bucket = "${var.aws_account}"
-    key    = "aws/terraform.tfstate"
-    region = "us-east-1"
+    bucket   = "${var.aws_account}"
+    key      = "aws/terraform.tfstate"
+    provider = "${var.aws_account}"
+    region   = "us-east-1"
   }
 }
 
@@ -12,8 +13,9 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config {
-    bucket = "${var.aws_account}"
-    key    = "aws/${var.aws_region}/${var.vpc_name}/terraform.tfstate"
-    region = "us-east-1"
+    bucket   = "${var.aws_account}"
+    key      = "aws/${var.aws_region}/${var.vpc_name}/terraform.tfstate"
+    provider = "${var.aws_account}"
+    region   = "us-east-1"
   }
 }
